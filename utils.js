5d865106528593c5b0011d4ce2638759076417b5
Nick Pitrak <pitrak1@gmail.com>
2019-08-31 20:54:54 -0500

    add getEquipment

--- utils.js
+++ utils.js
@@ -8,6 +8,35 @@ exports.createDbConnection = () => {
   return client
 }
 
+exports.getEquipment = async client => {
+  try {
+    const result = await client.query(`
+      SELECT
+        Equipments.id,
+        Equipments.serial_number,
+        Oems.id AS oem_id,
+        Oems.name AS oem_name,
+        Models.id AS model_id,
+        Models.name AS model_name,
+        Types.id AS type_id,
+        Types.name AS type_name
+      FROM Equipments
+        INNER JOIN Types ON Equipments.type_id = Types.id
+        INNER JOIN Models ON Equipments.model_id = Models.id
+        INNER JOIN Oems ON Models.oem_id = Oems.id;
+    `)
+    return {
+      statusCode: 200,
+      body: result.rows,
+    }
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    }
+  }
+}
+
 exports.createEquipment = async (client, fields) => {
   try {
     await client.query(`
