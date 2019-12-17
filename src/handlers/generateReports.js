1bfecce0e9b8247e81098d467bf6ebf933ba3f87
Nick Pitrak <pitrak1@gmail.com>
2019-12-17 09:57:26 -0600

    add equipment report

--- src/handlers/generateReports.js
+++ src/handlers/generateReports.js
@@ -4,24 +4,68 @@ const reportQueries = require('../queries/reportQueries')
 const client = utils.createDbConnection()
 
 exports.handler = async (event, _context, _callback) => {
+  const equipmentHandler = async () => {
+    const equipment = await client.query(reportQueries.getEquipment())
+    let result =
+      'serial_number, model, oem, type, notes, cal_company, cal_due, status, job_number, company_notes, start_date, end_date'
+
+    for (const item of equipment.rows) {
+      const serialNumber = item.serial_number.replace(/,/g, '')
+      const model = item.model_name.replace(/,/g, '')
+      const oem = item.oem_name.replace(/,/g, '')
+      const type = item.type_name.replace(/,/g, '')
+      const notes = item.notes ? item.notes.replace(/,/g, '') : ''
+      const calCompany = item.cal_company
+        ? item.cal_company.replace(/,/g, '')
+        : ''
+      const calDue = item.cal_due ? item.cal_due.replace(/,/g, '') : ''
+      const status = item.event_status.replace(/,/g, '')
+      const jobNumber = item.event_job_number
+        ? item.event_job_number.replace(/,/g, '')
+        : ''
+      const companyNotes = item.event_company_notes
+        ? item.event_company_notes.replace(/,/g, '')
+        : ''
+      const startDate = item.event_start_date
+        ? item.event_start_date.replace(/,/g, '')
+        : ''
+      const endDate = item.event_end_date
+        ? item.event_end_date.replace(/,/g, '')
+        : ''
+
+      result += `\n${serialNumber},${model},${oem},${type},${notes},${calCompany},${calDue},${status},${jobNumber},${companyNotes},${startDate},${endDate}`
+    }
+
+    return {
+      statusCode: 200,
+      body: { result },
+    }
+  }
+
+  const countHandler = async () => {
+    const itemGroups = await client.query(reportQueries.getItemGroups())
+    let result = 'handle,count'
+
+    for (const itemGroup of itemGroups.rows) {
+      const count = await client.query(reportQueries.getCount(itemGroup.id))
+      const handles = await client.query(reportQueries.getHandles(itemGroup.id))
+      handles.rows.forEach(handle => {
+        result += `\n${handle.handle},${count.rows[0].min || 0}`
+      })
+    }
+
+    return {
+      statusCode: 200,
+      body: { result },
+    }
+  }
+
   const handler = async event => {
     try {
-      const itemGroups = await client.query(reportQueries.getItemGroups())
-      let result = 'handle,count'
-
-      for (const itemGroup of itemGroups.rows) {
-        const count = await client.query(reportQueries.getCount(itemGroup.id))
-        const handles = await client.query(
-          reportQueries.getHandles(itemGroup.id),
-        )
-        handles.rows.forEach(handle => {
-          result += `\n${handle.handle},${count.rows[0].min || 0}`
-        })
-      }
-
-      return {
-        statusCode: 200,
-        body: { result },
+      if (event.reportName === 'equipment') {
+        return await equipmentHandler()
+      } else {
+        return await countHandler()
       }
     } catch (e) {
       return {
--- src/queries/reportQueries.js
+++ src/queries/reportQueries.js
@@ -19,3 +19,29 @@ exports.getHandles = id => `
   FROM Handles
   WHERE item_group_id = ${id};
 `
+
+exports.getEquipment = () => `
+  SELECT
+    RecentEvents.id AS event_id,
+    RecentEvents.status AS event_status,
+    RecentEvents.job_number AS event_job_number,
+    RecentEvents.company_notes AS event_company_notes,
+    RecentEvents.start_date AS event_start_date,
+    RecentEvents.end_date AS event_end_date,
+    Equipments.id AS id,
+    Equipments.serial_number,
+    Equipments.notes,
+    Equipments.cal_company,
+    Equipments.cal_due,
+    Types.id AS type_id,
+    Types.name AS type_name,
+    Models.id AS model_id,
+    Models.name AS model_name,
+    Oems.id AS oem_id,
+    Oems.name AS oem_name
+  FROM RecentEvents
+  RIGHT JOIN Equipments ON Equipments.id = RecentEvents.equipment_id
+  INNER JOIN Types ON Equipments.type_id = Types.id
+  INNER JOIN Models ON Equipments.model_id = Models.id
+  INNER JOIN Oems ON Models.oem_id = Oems.id
+`
