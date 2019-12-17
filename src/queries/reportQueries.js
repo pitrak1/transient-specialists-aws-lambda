a70c1611dac47e1043aeaf7fa5e2f1d41a3472c7
Nick Pitrak <pitrak1@gmail.com>
2019-12-16 18:56:03 -0600

    fix report query to properly count missing equipment

--- src/queries/reportQueries.js
+++ src/queries/reportQueries.js
@@ -5,11 +5,11 @@ exports.getItemGroups = () => `
 exports.getCount = id => `
   SELECT MIN(Counts.count)
   FROM (
-    SELECT COUNT(*), ItemGroupsModels.model_id
+    SELECT COUNT(CASE WHEN (RecentEvents.status = 'READY' OR RecentEvents.status = 'IN') THEN 1 END), ItemGroupsModels.model_id
     FROM Equipments
     INNER JOIN ItemGroupsModels ON Equipments.model_id = ItemGroupsModels.model_id
     LEFT JOIN RecentEvents ON Equipments.id = RecentEvents.equipment_id
-    WHERE ItemGroupsModels.item_group_id = ${id} AND (RecentEvents.status = 'READY' OR RecentEvents.status = 'IN')
+    WHERE ItemGroupsModels.item_group_id = ${id}
     GROUP BY ItemGroupsModels.model_id
   ) AS Counts;
 `
