7707d02903c71af9c57db811856283467b837cfc
Nick Pitrak <pitrak1@gmail.com>
2021-01-14 19:01:31 -0600

    change s3 cors origin

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -5,7 +5,7 @@ resource aws_s3_bucket "transient-specialists-files" {
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["HEAD", "GET", "PUT", "POST", "DELETE"]
-    allowed_origins = ["http://localhost:8080", "http://transientspecialistsinventory.com"]
+    allowed_origins = ["http://localhost:8080", "https://transientspecialistsinventory.com"]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
   }
